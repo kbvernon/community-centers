d256080e938f6a65817e35ba25d54ca79229aaf6
kbvernon <kenneth.b.vernon@gmail.com>
2024-02-14 11:29:24 -0700

    previous code stopped working, so refactored to use vrt directly, also dropped aggregation and saved at native resolution

--- R/get-elevation.R
+++ R/get-elevation.R
@@ -1,41 +1,44 @@
 
 # get elevation data
-
-library(FedData)
 library(here)
 library(purrr)
 library(sf)
 library(terra)
 
-download_dem <- function(region, geometry){
+download_dem <- function(x){
   
-  r <- geometry |> 
-    st_sfc(crs = 4326) |> 
-    get_ned(res = "1", label = region) |> 
-    rast() |> 
-    aggregate(fact = 3)
-  
-  names(r) <- region
-  
-  return(r)
+  # location of the virtual raster pointing to the
+  # USGS one arc second elevation data (~30 m resolution)
+  elevation_source <- file.path(
+    "https://prd-tnm.s3.amazonaws.com/StagedProducts",
+    "Elevation/1/TIFF",
+    "USGS_Seamless_DEM_1.vrt"
+  )
+
+  # connect to vrt
+  rr <- terra::rast(elevation_source, vsi = TRUE)
+
+  feature <- terra::vect(x)
+  feature <- terra::project(feature, terra::crs(rr))
+  feature <- terra::ext(feature)
+  feature <- terra::vect(feature, crs = terra::crs(rr))
+
+  fn <- paste0("dem-", x[["region"]], ".tif")
+
+  rr <- terra::crop(
+    rr,
+    feature,
+    snap = "out",
+    mask = TRUE,
+    filename = here("data", fn),
+    datatype = "FLT4S",
+    gdal = c("COMPRESS=DEFLATE", "ZLEVEL=9"),
+    overwrite = TRUE
+  )
   
 }
 
-write_dem <- function(x){
-  
-  fn <- here("data", paste0("dem-", names(x), ".tif"))
-  
-  writeRaster(x, filename = fn, overwrite = TRUE)
-  
-}
-
-# for some reason, values aren't read in after downloading the first time,
-# so have to call this twice
 here("data", "community-centers.gpkg") |> 
   read_sf(layer = "regions") |> 
-  pmap(download_dem)
-
-here("data", "community-centers.gpkg") |> 
-  read_sf(layer = "regions") |> 
-  pmap(download_dem) |> 
-  walk(write_dem)
+  split(1:3) |> 
+  walk(download_dem)
